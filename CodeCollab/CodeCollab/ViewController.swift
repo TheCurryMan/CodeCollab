4d44f44cd2e7422f1d927e8fc6d3642951f6f3c4
Avinash12388 <avinashkalpitjain@gmail.com>
2016-04-16 12:17:40 -0700

    iPhone app basic
    Nothing in it.

---
+++ CodeCollab/CodeCollab/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CodeCollab
+//
+//  Created by Avinash Jain on 4/16/16.
+//  Copyright © 2016 Avinash Jain. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
